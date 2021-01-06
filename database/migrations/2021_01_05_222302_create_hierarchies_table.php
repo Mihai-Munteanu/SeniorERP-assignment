44904581f6d5c26a2cbe850eb30ada89b9c1b681
Mihai Munteanu <mihai@Mihais-MacBook-Pro.local>
2021-01-06 13:33:20 +0200

    added a migration

---
+++ database/migrations/2021_01_05_222302_create_hierarchies_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHierarchiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hierarchies', function (Blueprint $table) {
+            $table->primary(['user_id', 'supervising_user_id']);
+            $table->foreignId('user_id');
+            $table->foreignId('supervising_user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('supervising_user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hierarchies');
+    }
+}
